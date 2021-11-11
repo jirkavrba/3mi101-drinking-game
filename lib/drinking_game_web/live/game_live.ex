3fc3a949e4e0148de33f275917ab3e46527934d1
Jiří Vrba <jiri@vrba.dev>
2021-11-11 21:02:48 +0100

    Remove unused parameter

--- lib/drinking_game_web/live/game_live.ex
+++ lib/drinking_game_web/live/game_live.ex
@@ -1,7 +1,7 @@
 defmodule DrinkingGameWeb.GameLive do
   use DrinkingGameWeb, :live_view
 
-  def mount(_params, session, socket) do
+  def mount(_params, _session, socket) do
     socket = socket
     |> assign(:game, DrinkingGame.GameServer.get_current_state())
     |> assign(:cup_drinked, false)
