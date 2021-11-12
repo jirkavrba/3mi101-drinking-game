673933e4586d80ed0d8e7ad4f085c39f138f0612
Jiří Vrba <jiri@vrba.dev>
2021-11-12 12:32:51 +0100

    Implement drinking available cup

--- lib/drinking_game_web/live/game_live.ex
+++ lib/drinking_game_web/live/game_live.ex
@@ -4,7 +4,7 @@ defmodule DrinkingGameWeb.GameLive do
   def mount(_params, _session, socket) do
     socket = socket
     |> assign(:game, DrinkingGame.GameServer.get_current_state())
-    |> assign(:cup_drinked, false)
+    |> assign(:cup_available, false)
 
     Phoenix.PubSub.subscribe(DrinkingGame.PubSub, "game_updates")
 
@@ -12,11 +12,21 @@ defmodule DrinkingGameWeb.GameLive do
   end
 
   def handle_info({:game_updated, game}, socket) do
-    {:noreply, assign(socket, :game, game)}
+    new_cup_available = rem(game.counter, game.increments_per_cup) == 0
+
+    socket = socket
+    |> assign(:game, game)
+    |> assign(:cup_available, socket.assigns.cup_available or new_cup_available)
+
+    {:noreply, socket}
   end
 
   def handle_event("increment", _params, socket) do
     DrinkingGame.GameServer.increment()
     {:noreply, socket}
+  end
+
+  def handle_event("drink_cup", _params, socket) do
+    {:noreply, assign(socket, :cup_available, false)}
   end
 end
